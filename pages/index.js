2a329c396ed87a6dae13dc82c8f253198e2c222c
Mahadari Sandeep <mahadarisandeep@gmail.com>
2022-04-18 18:55:08 +0530

    modified

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,17 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello,I'm Sandeep.I'm full stack developer.My website {' '}<a href='https://sandeepmichael.github.io'>Click Here</a></p>
-        
+        <p>Hello!i'm Mahadari Sandeep pursuing my fulltime engineering course
+          in branch of metallurgical engineering in JNTU hyderabad.
+
+          As a student some technical skills much needed.so i started to learn some
+          skills.most of the time i spend lot in code and popular JSlibrary and frameworks which are completely
+          guiding me to a developer in future.</p>
+          <a href='https://sandeepmichael.github.io'>My Website</a>
+
         <h3>
           <Link href='/posts/first-post'>
-          Next Page
+            Next Page
           </Link>
         </h3>
       </section>
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -21,10 +21,10 @@ export default function FirstPost() {
       <a href='https://secure-brook-86485.herokuapp.com/'>www.heroku.com</a>
       <p>This is the simple personalBlog created with Nodejs and it also have a connection to Mongo Database.so basically it's a full stack application.</p>
       <hr/>
-      <a href='https://vigilant-jackson-4c7134.netlify.app/'>www.heroku.com</a>
+      <a href='https://vigilant-jackson-4c7134.netlify.app/'>www.netlify.com</a>
       <p>This is the fourth project. in this project i used the react.js and also youtube api key to get the results This is same as like as the youtube app. when we search for any video the request will be sent to youtubeAPI and that api will send the respective results.</p>
       <hr/>
-      
+
 
     </Layout>)
 }
